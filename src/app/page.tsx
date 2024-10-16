c4e33e4dbeae425f0c5331444218760332e97775
adamcanard <azcunard@gmail.com>
2024-10-16 17:59:06 -0600

    Maybe fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
+import { useEffect, useState } from "react";
 import TopLevel from "./components/sitecomps/toplevel";
 import MobileTop from "./mobile/mobiletop";
 export default function Page() {
-  const mql = window.matchMedia("(max-width: 1000px)");
+  const [flag, setFlag] = useState(true);
+  useEffect(() => {
+    const mql = window.matchMedia("(max-width: 1000px)");
+    setFlag(mql.matches);
+  }, []);
 
   return (
     <>
-      {mql.matches ? (
+      {flag ? (
         <div id="mobile" className={"h-full flex-col"}>
           <MobileTop />
         </div>
