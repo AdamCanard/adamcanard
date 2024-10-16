ad3731707eea82394d0f495244df7e9aad7e5b77
adamcanard <azcunard@gmail.com>
2024-10-16 17:46:19 -0600

    Fixed mobile rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,21 @@
 import TopLevel from "./components/sitecomps/toplevel";
 import MobileTop from "./mobile/mobiletop";
 export default function Page() {
+  const mql = window.matchMedia("(max-width: 1000px)");
+
   return (
     <>
-      <div id="mobile" className={"h-full flex-col"}>
-        <MobileTop />
-      </div>
-      <div id="desktop">
-        <div className="flex justify-center items-center w-full h-full">
-          <TopLevel />
+      {mql.matches ? (
+        <div id="mobile" className={"h-full flex-col"}>
+          <MobileTop />
         </div>
-      </div>
+      ) : (
+        <div id="desktop">
+          <div className="flex justify-center items-center w-full h-full">
+            <TopLevel />
+          </div>
+        </div>
+      )}
     </>
   );
 }
