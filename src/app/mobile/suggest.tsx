5394628e6042495e709889932c709baf92418ba3
adamcanard <azcunard@gmail.com>
2024-10-22 10:09:14 -0600

    suggestion form

--- src/app/mobile/suggest.tsx
+++ src/app/mobile/suggest.tsx
@@ -1,3 +1,39 @@
+import { LabeledInputStr } from "../components/labeledinputs";
+import { useState } from "react";
 export default function Suggest() {
-  return <></>;
+  const [email, setEmail] = useState("");
+  const [beer, setBeer] = useState("");
+  const [brewery, setBrewery] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="boxshadow" className={"w-full"}>
+      <form autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
+        <LabeledInputStr
+          title="Email"
+          state={email}
+          setState={setEmail}
+          required={true}
+        />
+        <LabeledInputStr
+          title="Beer"
+          state={beer}
+          setState={setBeer}
+          required={true}
+        />
+        <LabeledInputStr
+          title="Brewery"
+          state={brewery}
+          setState={setBrewery}
+          required={true}
+        />
+        <div id="button-i">
+          <input id="button" type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
 }
