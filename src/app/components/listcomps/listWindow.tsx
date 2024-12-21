b3415f7020870f02b46d2d8285c237fde73df871
adamcanard <azcunard@gmail.com>
2024-12-20 18:02:04 -0700

    data now populates window

--- src/app/components/listcomps/listWindow.tsx
+++ src/app/components/listcomps/listWindow.tsx
@@ -8,7 +8,19 @@ export default function ListWindow(props: {
   console.log(props.data);
   return (
     <DesktopWindow title={props.id} width={"8rem"} height={""}>
-      <></>
+      {Object.values(props.data).map((data, index: number) => {
+        if ((data as string) !== "") {
+          return (
+            <div
+              id="border"
+              className="flex w-full h-full justify-between items-center p-3"
+              key={index + props.id}
+            >
+              {data as string}
+            </div>
+          );
+        }
+      })}
     </DesktopWindow>
   );
 }
