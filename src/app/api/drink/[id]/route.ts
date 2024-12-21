72220d5e54e360e522b1c50c969b76581c22c3c7
adamcanard <azcunard@gmail.com>
2024-12-20 17:40:35 -0700

    fixed api

--- src/app/api/drink/[id]/route.ts
+++ src/app/api/drink/[id]/route.ts
@@ -4,6 +4,6 @@ export async function GET(
   req: Request,
   { params }: { params: { id: string } },
 ) {
-  const data = await db.getById("Beers", params.id);
+  const data = await db.getById("Beer", params.id);
   return new Response(JSON.stringify(data), { status: 200 });
 }
