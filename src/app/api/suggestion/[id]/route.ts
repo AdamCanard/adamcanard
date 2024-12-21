9a753fb695fa09fe328410d3610a2d99049d7e7f
adamcanard <azcunard@gmail.com>
2024-12-20 17:37:11 -0700

    new dynamic api endpoint for lists

---
+++ src/app/api/suggestion/[id]/route.ts
@@ -0,0 +1,9 @@
+import db from "@/app/server/pb";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const data = await db.getById("Suggestion", params.id);
+  return new Response(JSON.stringify(data), { status: 200 });
+}
