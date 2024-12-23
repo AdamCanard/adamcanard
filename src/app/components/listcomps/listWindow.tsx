137248060d65aa5682b2f0d94f8d29f2edf63b93
adamcanard <azcunard@gmail.com>
2024-12-23 12:25:24 -0700

    add update conditional and input windows

--- src/app/components/listcomps/listWindow.tsx
+++ src/app/components/listcomps/listWindow.tsx
@@ -1,16 +1,49 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import DesktopWindow from "../sitecomps/desktopwindow";
 import { TaskbarContext } from "../sitecomps/toplevel";
+import Input from "./input";
 
 export default function ListWindow(props: {
   api: string;
   data: object;
   id: string;
 }) {
+  const [updating, setUpdating] = useState(false);
   const { windows, setWindows, admin } = useContext(TaskbarContext);
   const keys = Object.keys(props.data);
 
-  const updateItem = () => {};
+  const updateItem = () => {
+    setUpdating(true);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    await postData(formData);
+  };
+
+  const postData = async (formData: FormData) => {
+    try {
+      const response = await fetch(props.api, {
+        method: "PUT",
+        body: formData,
+      });
+      return response;
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return new Response(
+          JSON.stringify({ error: err.message || err.toString() }),
+          {
+            status: 500,
+            headers: {},
+          },
+        );
+      } else {
+        console.log(err);
+      }
+    }
+  };
 
   const deleteItem = () => {
     deleteById(props.id);
@@ -43,35 +76,67 @@ export default function ListWindow(props: {
   return (
     <DesktopWindow
       title={Object.values(props.data)[0]}
-      width={"16rem"}
+      width={"18rem"}
       height={""}
     >
-      {Object.values(props.data).map((data, index: number) => {
-        if (!Omit.includes(keys[index])) {
-          if ((data as string) !== "" && index !== 0) {
-            return (
-              <div
-                id="border"
-                className="flex w-full h-full justify-between items-center p-3"
-                key={index + props.id}
-              >
-                <div>{keys[index]}:</div>
-                <div>{data as string}</div>
+      {!updating ? (
+        <>
+          {Object.values(props.data).map((data, index: number) => {
+            if (!Omit.includes(keys[index])) {
+              if ((data as string) !== "" && index !== 0) {
+                return (
+                  <div
+                    id="border"
+                    className="flex w-full h-full justify-between items-center p-3"
+                    key={index + props.id}
+                  >
+                    <div>{keys[index]}:</div>
+                    <div>{data as string}</div>
+                  </div>
+                );
+              }
+            }
+          })}
+          {admin && (
+            <div id="button-i">
+              <div onClick={updateItem} id="button">
+                Update
               </div>
-            );
-          }
-        }
-      })}
-      {admin && (
-        <div id="button-i">
-          <div onClick={updateItem} id="button">
-            Update
-          </div>
 
-          <div onClick={deleteItem} id="button">
-            Delete
+              <div onClick={deleteItem} id="button">
+                Delete
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <form
+          className="flex flex-col"
+          onSubmit={(e) => handleSubmit(e)}
+          autoComplete="off"
+        >
+          {Object.values(props.data).map((data, index: number) => {
+            if (!Omit.includes(keys[index])) {
+              return (
+                <Input name={keys[index]} value={data} key={index + props.id} />
+              );
+            }
+          })}
+          <div id="button-i">
+            <div
+              onClick={() => {
+                setUpdating(false);
+              }}
+              id="button"
+            >
+              Cancel
+            </div>
+
+            <div onClick={deleteItem} id="button">
+              Save
+            </div>
           </div>
-        </div>
+        </form>
       )}
     </DesktopWindow>
   );
