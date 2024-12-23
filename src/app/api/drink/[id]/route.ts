b3bec118c6605953b48485d048b4395db8a7b539
adamcanard <azcunard@gmail.com>
2024-12-22 20:17:05 -0700

    added api for deletes

--- src/app/api/drink/[id]/route.ts
+++ src/app/api/drink/[id]/route.ts
@@ -7,3 +7,10 @@ export async function GET(
   const data = await db.getById("Beer", params.id);
   return new Response(JSON.stringify(data), { status: 200 });
 }
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const data = await db.delete("Beer", params.id);
+  return new Response(JSON.stringify(data), { status: 200 });
+}
