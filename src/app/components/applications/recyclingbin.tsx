06b4ecb3012249525446025378a1557a2ab874c1
adamcanard <azcunard@gmail.com>
2024-12-23 19:03:35 -0700

    added suggestions in recycling bin

--- src/app/components/applications/recyclingbin.tsx
+++ src/app/components/applications/recyclingbin.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import DesktopWindow from "../sitecomps/desktopwindow";
 
 export default function RecyclingBin() {
+  const [suggestion, setSuggestion] = useState("");
   return (
     <DesktopWindow title="Recycling Bin" width="20rem" height="">
-      <></>
+      <form className={"w-full h-full"}>
+        <label id="border" className="flex justify-between w-full text-sm">
+          Enter Suggestion:
+          <textarea
+            autoComplete="off"
+            required
+            name={"Suggestions"}
+            value={suggestion}
+            onChange={(e) => setSuggestion(e.target.value)}
+            className={"h-12"}
+          />
+        </label>
+        <div id="button-i">
+          <input id="button" type="submit" value="Submit" />
+        </div>
+      </form>
     </DesktopWindow>
   );
 }
--- src/app/components/sitecomps/desktopapplications.tsx
+++ src/app/components/sitecomps/desktopapplications.tsx
@@ -3,11 +3,7 @@ import Application from "./application";
 
 export default function DesktopApplications() {
   return (
-    <div
-      className={
-        "grid border-white border-2 w-full h-full grid-cols-9 grid-rows-5"
-      }
-    >
+    <div className={"grid w-full h-full grid-cols-9 grid-rows-5"}>
       <Application
         title="Recycling Bin"
         src=""
