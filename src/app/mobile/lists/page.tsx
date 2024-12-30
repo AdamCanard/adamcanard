c83d2256420b9e868936308009b722fd391c65ee
adamcanard <azcunard@gmail.com>
2024-12-30 11:33:01 -0700

    final fix

--- src/app/mobile/lists/page.tsx
+++ src/app/mobile/lists/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Page() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/mobile/lists/drank");
+    router.push("/mobile/lists/beer");
   });
   return <></>;
 }
