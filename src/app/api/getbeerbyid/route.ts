1cbe155d6fa32ff7dd946dcd3faee99624b2ee03
adamcanard <azcunard@gmail.com>
2025-01-02 17:38:01 -0700

    removed useless apis

--- src/app/api/getbeerbyid/route.ts
+++
@@ -1,8 +0,0 @@
-import db from "../../server/pb";
-
-export async function POST(req: Request) {
-  const formData = await req.formData();
-  const id = formData.get("id") as string;
-  const beer = await db.getById("Beer", id);
-  return new Response(JSON.stringify(beer), { status: 200 });
-}
