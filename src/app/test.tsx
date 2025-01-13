e21549b2c8b61ed47cbd94d4bced45f496f56321
adamcanard <azcunard@gmail.com>
2025-01-13 16:33:22 -0700

    fixed global window issue

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -5,7 +5,7 @@ import DesktopWindow from "./desktop/sitecomps/desktopwindow";
 export default function Test() {
   return (
     <DesktopWindow title="Test" width="24rem" height="16rem">
-      <></>
+      <div id="boxshadow" className={"w-full h-full"}></div>
     </DesktopWindow>
   );
 }
