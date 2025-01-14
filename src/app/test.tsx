7e832c0f1533be9a7d94de4236841790fa081a7b
adamcanard <azcunard@gmail.com>
2025-01-14 12:41:19 -0700

    finalized test, added new css for pressed windows icon

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -2,12 +2,8 @@
 
 import Image from "next/image";
 import DesktopWindow from "./desktop/sitecomps/desktopwindow";
-import { useRouter } from "next/navigation";
-import MBlackJackGame from "./desktop/blackjackcomps/mobileblackjack";
 
 export default function Test() {
-  const router = useRouter();
-
   return (
     <DesktopWindow title="Test" width="20rem" height="32rem">
       <div className={"flex flex-col w-full h-full"}>
@@ -17,43 +13,24 @@ export default function Test() {
           </div>
         </div>
 
-        <div id="boxshadow" className={"flex flex-col w-full h-32"}>
-          <h1 id="title">Information</h1>
-          <div id="boxshadowNP" className={"flex flex-col w-full h-full"}>
-            <div className={"w-full h-full px-2 text-sm"}>
-              If you want to see what im really capable of, visit this site on
-              desktop! In the meantime, play some BlackJack or check out my
-              links
-            </div>
-            <div
-              className={"flex w-full gap-2 justify-end items-end pb-1 pr-1"}
-            >
-              <a href="https://www.instagram.com/adam_cunard/" id="button">
-                Instagram
-              </a>
-              <a
-                href="https://ca.linkedin.com/in/adam-cunard-3a4644287?trk=people-guest_people_search-card"
-                id="button"
-              >
-                LinkedIn
-              </a>
-              <a href="https://github.com/AdamCanard" id="button">
-                GitHub
-              </a>
-            </div>
-          </div>
-        </div>
-        <div id="boxshadow" className={"w-full flex flex-col"}>
-          <h1
-            id="title"
-            onClick={() => {
-              router.push("/mobile/secret");
-            }}
+        <div id="boxshadow" className={"flex flex-col w-full"}>
+          <h1 id="title">Links</h1>
+          <div
+            className={"w-full h-full flex flex-row justify-end items-center"}
           >
-            {" "}
-            BlackJack
-          </h1>
-          <MBlackJackGame />
+            <a href="https://www.instagram.com/adam_cunard/" id="button">
+              Instagram
+            </a>
+            <a
+              href="https://ca.linkedin.com/in/adam-cunard-3a4644287?trk=people-guest_people_search-card"
+              id="button"
+            >
+              LinkedIn
+            </a>
+            <a href="https://github.com/AdamCanard" id="button">
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </DesktopWindow>
