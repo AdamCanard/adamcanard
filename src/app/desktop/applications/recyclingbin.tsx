516c4cb5736515c710908402a5d7561f9e8ecc70
adamcanard <azcunard@gmail.com>
2025-01-14 13:05:45 -0700

    recycling bin finally works

--- src/app/desktop/applications/recyclingbin.tsx
+++ src/app/desktop/applications/recyclingbin.tsx
@@ -4,15 +4,47 @@ import DesktopWindow from "../sitecomps/desktopwindow";
 
 export default function RecyclingBin() {
   const [suggestion, setSuggestion] = useState("");
+  const postData = async (formData: FormData) => {
+    try {
+      const response = await fetch("/api/application/" + "Recycling", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+
+      return data;
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return new Response(
+          JSON.stringify({ error: err.message || err.toString() }),
+          {
+            status: 500,
+            headers: {},
+          },
+        );
+      } else {
+        console.log(err);
+      }
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    await postData(formData);
+    setSuggestion("");
+  };
   return (
     <DesktopWindow title="Recycling Bin" width="20rem" height="">
-      <form className={"w-full h-full"}>
+      <form className={"w-full h-full"} onSubmit={(e) => handleSubmit(e)}>
         <label id="border" className="flex justify-between w-full text-sm">
           Enter Suggestion:
           <textarea
             autoComplete="off"
             required
-            name={"Suggestions"}
+            name={"Suggestion"}
             value={suggestion}
             onChange={(e) => setSuggestion(e.target.value)}
             className={"h-12"}
