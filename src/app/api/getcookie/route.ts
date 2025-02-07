d5fa601451166387ce449974a2ade22c17f566d4
Jeremy Cox <93951833+JeremyCCox@users.noreply.github.com>
2025-02-06 23:16:29 -0700

    Delete src directory

--- src/app/api/getcookie/route.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-export async function POST(req: Request) {
-  const formData = await req.formData();
-  const cookie = formData.get("cookie") as string;
-  const data = cookies().get(cookie);
-  return new Response(JSON.stringify({ data: data }), {
-    status: 200,
-  });
-}
--- src/app/collections.tsx
+++
@@ -1,7 +0,0 @@
-export const Collections: Record<string, boolean> = {
-  Beers: false,
-  Suggestion: true,
-  Ideas: false,
-  Books: false,
-  Vinyls: false,
-};
