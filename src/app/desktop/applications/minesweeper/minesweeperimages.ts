048bc926c66ed6f6d5869675c904fff27f27536c
adamcanard <azcunard@gmail.com>
2025-02-14 15:16:47 -0700

    fe: added sevensegment display images

--- src/app/desktop/applications/minesweeper/minesweeperimages.ts
+++ src/app/desktop/applications/minesweeper/minesweeperimages.ts
@@ -10,7 +10,18 @@ import seven from "../../../../../public/minesweeper/Minesweeper_7.png";
 import eight from "../../../../../public/minesweeper/Minesweeper_8.png";
 import bomb from "../../../../../public/minesweeper/bomb.png";
 
-const minesweeperImages: Record<string, StaticImageData> = {
+import SevenSeg0 from "../../../../../public/minesweeper/timer/7Seg0.png";
+import SevenSeg1 from "../../../../../public/minesweeper/timer/7Seg1.png";
+import SevenSeg2 from "../../../../../public/minesweeper/timer/7Seg2.png";
+import SevenSeg3 from "../../../../../public/minesweeper/timer/7Seg3.png";
+import SevenSeg4 from "../../../../../public/minesweeper/timer/7Seg4.png";
+import SevenSeg5 from "../../../../../public/minesweeper/timer/7Seg5.png";
+import SevenSeg6 from "../../../../../public/minesweeper/timer/7Seg6.png";
+import SevenSeg7 from "../../../../../public/minesweeper/timer/7Seg7.png";
+import SevenSeg8 from "../../../../../public/minesweeper/timer/7Seg8.png";
+import SevenSeg9 from "../../../../../public/minesweeper/timer/7Seg9.png";
+
+export const minesweeperImages: Record<string, StaticImageData> = {
   "0": zero,
   "1": one,
   "2": two,
@@ -23,4 +34,15 @@ const minesweeperImages: Record<string, StaticImageData> = {
   F: bomb,
 };
 
-export default minesweeperImages;
+export const timerListImages = [
+  SevenSeg0,
+  SevenSeg1,
+  SevenSeg2,
+  SevenSeg3,
+  SevenSeg4,
+  SevenSeg5,
+  SevenSeg6,
+  SevenSeg7,
+  SevenSeg8,
+  SevenSeg9,
+];
