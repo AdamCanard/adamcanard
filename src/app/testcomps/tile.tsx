13143c653fd51b7581e8e35892d884ed9d165325
adamcanard <azcunard@gmail.com>
2025-02-18 20:28:21 -0700

    fe: made colors for tiles and types

--- src/app/testcomps/tile.tsx
+++ src/app/testcomps/tile.tsx
@@ -1,8 +1,17 @@
 import { ITileObject } from "./gamecontainer";
 
 export default function Tile(props: { tileObj: ITileObject }) {
+  const getColour = (value: string) => {
+    switch (value) {
+      case "E":
+        return "EmptyTile";
+      case "P":
+        return "PlayerTile";
+    }
+  };
   return (
     <div
+      id={getColour(props.tileObj.value)}
       className={
         "flex text-center justify-center items-center flex-wrap w-8 h-8 border-2"
       }
