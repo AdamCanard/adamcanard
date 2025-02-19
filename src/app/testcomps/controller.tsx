5ecf9d0456c8fd03c69c197a0217790553de19d3
adamcanard <azcunard@gmail.com>
2025-02-19 09:29:26 -0700

    fe: positioned buttons

--- src/app/testcomps/controller.tsx
+++ src/app/testcomps/controller.tsx
@@ -1,54 +1,72 @@
 export default function Controller(props: { move: (arg0: string) => void }) {
   return (
-    <div className={"h-64"}>
-      <div className={"grid grid-cols-3 grid-rows-3 w-24 h-16"}>
+    <div className={"h-52 flex flex-col"}>
+      <div className={"flex flex-row justify-around items-center pt-8"}>
         {" "}
+        <div className={"grid grid-cols-3 grid-rows-3 w-24 h-16"}>
+          {" "}
+          <button
+            className={
+              "col-start-2  row-start-1 justify-center items-center flex w-8 h-full border-2"
+            }
+            onClick={() => props.move("u")}
+          >
+            U
+          </button>
+          <button
+            className={
+              "col-start-2  row-start-3  justify-center items-center flex w-8 h-full border-2"
+            }
+            onClick={() => props.move("d")}
+          >
+            D
+          </button>
+          <button
+            className={
+              "col-start-2 row-start-2 justify-center items-center flex w-8 h-full border-2"
+            }
+          ></button>
+          <button
+            className={
+              "col-start-1 row-start-2 justify-center items-center flex w-8 h-full border-2"
+            }
+            onClick={() => props.move("l")}
+          >
+            L
+          </button>
+          <button
+            className={
+              "col-start-3 row-start-2 justify-center items-center flex w-8 h-full border-2"
+            }
+            onClick={() => props.move("r")}
+          >
+            R{" "}
+          </button>
+        </div>
         <button
-          className={
-            "col-start-2  row-start-1 justify-center items-center flex w-8 h-full border-2"
-          }
-          onClick={() => props.move("u")}
+          id="button"
+          className={"justify-center items-center flex w-1/2 h-16 border-2"}
+          onClick={() => props.move("a")}
         >
-          U
-        </button>
-        <button
-          className={
-            "col-start-2  row-start-3  justify-center items-center flex w-8 h-full border-2"
-          }
-          onClick={() => props.move("d")}
-        >
-          D
-        </button>
-        <button
-          className={
-            "col-start-2 row-start-2 justify-center items-center flex w-8 h-full border-2"
-          }
-        ></button>
-        <button
-          className={
-            "col-start-1 row-start-2 justify-center items-center flex w-8 h-full border-2"
-          }
-          onClick={() => props.move("l")}
-        >
-          L
-        </button>
-        <button
-          className={
-            "col-start-3 row-start-2 justify-center items-center flex w-8 h-full border-2"
-          }
-          onClick={() => props.move("r")}
-        >
-          R{" "}
+          A
         </button>
       </div>
-
-      <button
-        id="button"
-        className={"justify-center items-center flex w-1/2 h-16 border-2"}
-        onClick={() => props.move("a")}
+      <div
+        className={"flex flex-row gap-2 justify-center items-end h-full pb-2"}
       >
-        A
-      </button>
+        <button
+          className={"justify-center items-center flex w-6 h-6 border-2"}
+          onClick={() => props.move("sel")}
+        >
+          Sel
+        </button>
+        <button
+          className={"justify-center items-center flex w-6 h-6 border-2"}
+          onClick={() => props.move("sta")}
+        >
+          Sta
+        </button>
+      </div>
     </div>
   );
 }
