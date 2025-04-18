63c43b9e35b17e2c507aa06bccd6674724096a81
adamcanard <azcunard@gmail.com>
2025-04-18 12:53:25 -0600

    feat: added references

--- src/app/capstone/references/page.tsx
+++ src/app/capstone/references/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <></>;
+  return (
+    <div className={"h-full w-full"}>
+      <p id="border" className={"text-xl"}>
+        References:{" "}
+      </p>
+      <p id="border" className={"h-full"}></p>
+    </div>
+  );
 }
