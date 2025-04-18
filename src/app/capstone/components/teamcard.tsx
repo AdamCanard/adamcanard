49b68646e18a3fe4c2bd43cacf17c093f3514caa
adamcanard <azcunard@gmail.com>
2025-04-18 12:37:35 -0600

    again

--- src/app/capstone/components/teamcard.tsx
+++ src/app/capstone/components/teamcard.tsx
@@ -20,30 +20,18 @@ export default function TeamCard(props: {
               "w-full flex flex-col h-full justify-center items-center"
             }
           >
-            <div className={"flex flex-row h-[400px] "}>
-              <Image
-                className={"w-32 h-full"}
-                id="border"
-                src={props.image}
-                alt="Team Image"
-              />
+            <div className={"flex flex-row h-full "}>
               <div className={"flex flex-col w-full h-full"}>
-                <div className={"h-full"}>
-                  <h2 id="border" className={"text-3xl"}>
-                    {props.name}
-                  </h2>{" "}
-                  <p id="border" className={" text-lg"}>
-                    {props.contribution}
-                  </p>
-                  <p id="border" className={""}>
-                    {props.desc}
-                  </p>
-                </div>
-                <div
-                  className={
-                    "flex flex-row w-full bottom-0 justify-end items-end"
-                  }
-                >
+                <h2 id="border" className={"text-3xl"}>
+                  {props.name}
+                </h2>{" "}
+                <Image
+                  className={"w-full h-full"}
+                  id="border"
+                  src={props.image}
+                  alt="Team Image"
+                />
+                <div className={"flex flex-row w-full justify-around "}>
                   <Link
                     id="button"
                     href={props.linkedin}
@@ -54,6 +42,16 @@ export default function TeamCard(props: {
                   <Link id="button" href={props.github} className={"text-lg"}>
                     Github
                   </Link>
+                </div>
+              </div>
+              <div className={"flex flex-col w-full h-full"}>
+                <div className={"h-full"}>
+                  <p id="border" className={" "}>
+                    {props.contribution}
+                  </p>
+                  <p id="border" className={""}>
+                    {props.desc}
+                  </p>
                 </div>
               </div>
             </div>
