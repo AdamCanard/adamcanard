1f76450db92a64222824f4e3948f042eac6678cd
adamcanard <azcunard@gmail.com>
2025-04-20 14:46:51 -0600

    maybe?

--- src/app/capstone/references/page.tsx
+++ src/app/capstone/references/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>Sait PNG:</span> SAIT
           Catalyst Symbol. [Online image]. (n.d.). SAIT Our Brand.
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://www.sait.ca/about-sait/our-brand"}
           >
             https://www.sait.ca/about-sait/our-brand
@@ -28,7 +28,7 @@ export default function Page() {
           (n.d.). C programming language logo Microsoft Visual Studio .NET
           Framework [PNG image]. ImgBin.
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={
               "https://imgbin.com/png/BTmFxA0c/c-programming-language-logo-microsoft-visual-studio-net-framework-png"
             }
@@ -43,7 +43,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>.NET PNG:</span> Wikimedia
           Commons. (n.d.). File: .NET Core Logo.svg. Wikimedia Foundation.{" "}
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg"}
           >
             https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg{" "}
@@ -56,7 +56,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>Next JS PNG:</span> Iconduck.
           (n.d.). Next.js icon.
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://iconduck.com/icons/94662/nextjs"}
           >
             https://iconduck.com/icons/94662/nextjs{" "}
@@ -69,7 +69,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>React PNG:</span> Wikipedia.
           (n.d.). File: React-icon.svg. Wikimedia Foundation.
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://en.m.wikipedia.org/wiki/File:React-icon.svg"}
           >
             https://en.m.wikipedia.org/wiki/File:React-icon.svg{" "}
@@ -82,7 +82,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>PostgreSQL PNG:</span>{" "}
           CleanPNG. (n.d.). PostgreSQL transparent background PNG.{" "}
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://www.cleanpng.com/free/postgresql.html"}
           >
             https://www.cleanpng.com/free/postgresql.html
@@ -95,7 +95,7 @@ export default function Page() {
           <span className={"font-bold text-start"}>EF Core PNG:</span> McKenzie,
           D. (2020, May 18). Porting to Entity Framework Core. CodeOpinion.
           <Link
-            className={"text-blue-600 text-wrap"}
+            className={"text-blue-600 text-wrap overflow-scroll"}
             href={"https://codeopinion.com/porting-to-entity-framework-core/"}
           >
             https://codeopinion.com/porting-to-entity-framework-core/
