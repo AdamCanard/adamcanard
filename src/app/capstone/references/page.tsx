772306bddf88cbced50c4fab2113532798a4ef24
adamcanard <azcunard@gmail.com>
2025-04-20 14:25:00 -0600

    feat: references

--- src/app/capstone/references/page.tsx
+++ src/app/capstone/references/page.tsx
@@ -1,10 +1,86 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div className={"h-full w-full"}>
       <p id="border" className={"text-xl"}>
         References:{" "}
       </p>
-      <p id="border" className={"h-full"}></p>
+      <div id="border" className={"h-full"}>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>Sait PNG:</span> SAIT
+          Catalyst Symbol. [Online image]. (n.d.). SAIT Our Brand.
+          <Link
+            className={"text-blue-600"}
+            href={"https://www.sait.ca/about-sait/our-brand"}
+          >
+            https://www.sait.ca/about-sait/our-brand
+          </Link>
+        </p>{" "}
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>C# PNG:</span> imgbin.com.
+          (n.d.). C programming language logo Microsoft Visual Studio .NET
+          Framework [PNG image]. ImgBin.
+          <Link
+            className={"text-blue-600"}
+            href={
+              "https://imgbin.com/png/BTmFxA0c/c-programming-language-logo-microsoft-visual-studio-net-framework-png"
+            }
+          >
+            https://imgbin.com/png/BTmFxA0c/c-programming-language-logo-microsoft-visual-studio-net-framework-png
+          </Link>
+        </p>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>.NET PNG:</span> Wikimedia
+          Commons. (n.d.). File: .NET Core Logo.svg. Wikimedia Foundation.{" "}
+          <Link
+            className={"text-blue-600"}
+            href={"https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg"}
+          >
+            https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg{" "}
+          </Link>
+        </p>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>Next JS PNG:</span> Iconduck.
+          (n.d.). Next.js icon.
+          <Link
+            className={"text-blue-600"}
+            href={"https://iconduck.com/icons/94662/nextjs"}
+          >
+            https://iconduck.com/icons/94662/nextjs{" "}
+          </Link>
+        </p>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>React PNG:</span> Wikipedia.
+          (n.d.). File: React-icon.svg. Wikimedia Foundation.
+          <Link
+            className={"text-blue-600"}
+            href={"https://en.m.wikipedia.org/wiki/File:React-icon.svg"}
+          >
+            https://en.m.wikipedia.org/wiki/File:React-icon.svg{" "}
+          </Link>
+        </p>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>PostgreSQL PNG:</span>{" "}
+          CleanPNG. (n.d.). PostgreSQL transparent background PNG.{" "}
+          <Link
+            className={"text-blue-600"}
+            href={"https://www.cleanpng.com/free/postgresql.html"}
+          >
+            https://www.cleanpng.com/free/postgresql.html
+          </Link>
+        </p>
+        <p id="border" className={"flex flex-row justify-between text-center "}>
+          <span className={"font-bold text-start"}>EF Core PNG:</span> McKenzie,
+          D. (2020, May 18). Porting to Entity Framework Core. CodeOpinion.
+          <Link
+            className={"text-blue-600"}
+            href={"https://codeopinion.com/porting-to-entity-framework-core/"}
+          >
+            https://codeopinion.com/porting-to-entity-framework-core/
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
