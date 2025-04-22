a8712d5742f9bf3719788b1e2bdf9c8e8eec9be3
adamcanard <azcunard@gmail.com>
2025-04-22 10:25:47 -0600

    style: fixed styles

--- src/app/capstone/layout.tsx
+++ src/app/capstone/layout.tsx
@@ -8,9 +8,9 @@ export default function CapstoneLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div id="capstone" className={"h-dvh w-dvw flex flex-col"}>
+    <div className={"h-dvh w-dvw flex flex-col"}>
       <CapstoneHeader />
-      <div className={"flex h-full"}> {children}</div>
+      {children}{" "}
     </div>
   );
 }
