2a9998bb4c567c95efa967ace7175c9ee69d44cd
adamcanard <azcunard@gmail.com>
2025-04-27 15:40:28 -0600

    refactor: removed capstone qrs

--- src/app/capstone/demo/components/desktop/renderer/screenrenderer.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { RenderContext } from "./renderer";
-
-export function ScreenRenderer() {
-  const { windowToRender } = useContext(RenderContext);
-  return <>{windowToRender}</>;
-}
