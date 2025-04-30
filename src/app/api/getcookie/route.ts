69996f6ada2578ccef79d2d064300338414aeb6b
adamcanard <azcunard@gmail.com>
2025-04-30 13:53:08 -0600

    feat: improved get cookie api

--- src/app/api/getcookie/route.ts
+++ src/app/api/getcookie/route.ts
@@ -1,9 +1,18 @@
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const formData = await req.formData();
   const cookie = formData.get("cookie") as string;
-  const data = cookies().get(cookie);
-  return new Response(JSON.stringify({ data: data }), {
+  const data = cookies().get(cookie)?.value;
+  if (data === undefined) {
+    return NextResponse.json(
+      { message: "No cookie with name: " + cookie },
+      { status: 404 },
+    );
+  }
+  const returnCookie: Record<string, string> = {};
+  returnCookie[cookie as keyof object] = data;
+  return new NextResponse(JSON.stringify(returnCookie), {
     status: 200,
   });
 }
