90b506cc420233356cc522c7aa49e230007e1469
adamcanard <azcunard@gmail.com>
2025-05-29 21:04:27 -0600

    feat: background on bees

--- src/app/mobile/beer.tsx
+++ src/app/mobile/beer.tsx
@@ -120,7 +120,11 @@ export default function Beer() {
         back,
       }}
     >
-      <div className={"flex flex-col w-full h-full relative overflow-y-hidden"}>
+      <div
+        className={
+          "bg-[#c6c6c6] flex flex-col w-full h-full relative overflow-y-hidden"
+        }
+      >
         {render}
       </div>
     </BeerContext.Provider>
