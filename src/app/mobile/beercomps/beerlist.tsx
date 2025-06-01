b94a5dcc62d253453c325b7485fb6ab64e300a12
adamcanard <azcunard@gmail.com>
2025-06-01 11:19:45 -0600

    fix: display no beers after 5 seconds

--- src/app/mobile/beercomps/beerlist.tsx
+++ src/app/mobile/beercomps/beerlist.tsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BeerContext } from "../beer";
 import { IBeer } from "@/app/server/models/beer";
 import BeerElement from "./beerelement";
 import Filter from "./filter";
 
 export default function BeerList() {
+  const [empty, setEmpty] = useState(false);
   const { filter, keywords, beers } = useContext(BeerContext);
   const inSearch = (beer: IBeer) => {
     const brewery = filter["brewery"];
@@ -18,12 +19,25 @@ export default function BeerList() {
     }
     return false;
   };
+  useEffect(() => {
+    console.log("one");
+    const interval = setInterval(() => {
+      console.log("two");
+      setEmpty(true);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={"flex flex-col h-full overflow-y-auto"}>
       <div className="w-full flex flex-col h-full">
         {beers.length === 0 ? (
           <div className={" flex justify-center items-center w-full h-full"}>
-            <div className={"Border"}>Loading Beers...</div>{" "}
+            {empty ? (
+              <div className={"Border"}>No Beers Found</div>
+            ) : (
+              <div className={"Border"}>Loading Beers...</div>
+            )}
           </div>
         ) : (
           <>
