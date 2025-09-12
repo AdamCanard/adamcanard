e8388cdfed192b16698d6a0658e28489e0669668
adamcanard <azcunard@gmail.com>
2025-09-12 11:37:37 -0600

    feat: taskbar init

--- src/app/test/taskbar.tsx
+++ src/app/test/taskbar.tsx
@@ -1,3 +1,35 @@
+import { useContext } from "react";
+import { WindowContext } from "./windowprovider";
+import { IWindow } from "./windowrecord";
+import Image from "next/image";
+
 export default function TaskBar() {
-  return <div className={"w-full h-16 bg-black"}></div>;
+  const { windows, closeWindow } = useContext(WindowContext);
+  return (
+    <div className={"w-full h-8 bg-blue-400"}>
+      {" "}
+      {windows.map((window: IWindow) => {
+        return (
+          <div
+            onClick={() => closeWindow(window.window.key || "")}
+            key={window.window.key}
+            className={
+              "flex flex-row bg-blue-300 w-36 h-full p-2 gap-2 items-center"
+            }
+          >
+            <div className={"relative h-full w-1/8"}>
+              {" "}
+              <Image
+                src={window.icon}
+                alt={window.window.key + " Icon"}
+                fill={true}
+              />
+            </div>
+
+            <p>{window.window.key}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/app/test/windowrecord.tsx
+++ src/app/test/windowrecord.tsx
@@ -1,10 +1,26 @@
 import { ReactElement } from "react";
 import MobilePage from "../mobile/mobilepage";
+import Image, { StaticImageData } from "next/image";
+
+import MobileIcon from "../../../public/Windows/MobileIcon.png";
+
 export interface IWindow {
   window: ReactElement;
   width: number;
   height: number;
+  icon: StaticImageData;
 }
 export const windowRecord: Record<string, IWindow> = {
-  Mobile: { window: <MobilePage key={"Mobile"} />, width: 24, height: 48 },
+  Mobile: {
+    window: <MobilePage key={"Mobile"} />,
+    width: 24,
+    height: 48,
+    icon: MobileIcon,
+  },
+  Test: {
+    window: <Image src={MobileIcon} alt={" Icon"} fill={true} key={"apple"} />,
+    width: 16,
+    height: 16,
+    icon: MobileIcon,
+  },
 };
