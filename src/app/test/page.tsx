573ac5e3961a9cfc36ef160031edc756cffe677d
adamcanard <azcunard@gmail.com>
2025-09-15 15:14:25 -0600

    feat: progess

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import TaskBar from "./taskbar";
 import { WindowContext } from "./windowprovider";
 import Windows from "./windows";
+import { windowRecord } from "./windowrecord";
 
 export default function Page() {
   return (
@@ -18,5 +19,23 @@ export default function Page() {
 function StartMenu() {
   const { startMenu } = useContext(WindowContext);
   if (startMenu)
-    return <div className={"absolute w-96 h-1/2 bg-black bottom-8"}></div>;
+    return (
+      <div className={"flex flex-col absolute w-96 h-1/2 bg-blue-500 bottom-8"}>
+        <div
+          className={"grid grid-cols-3 grid-rows-4 h-full w-full bg-blue-200"}
+        >
+          {Object.values(windowRecord).map((window) => {
+            return (
+              <div
+                className={"w-full h-full border-2 border-black"}
+                key={window.window.key}
+              >
+                {window.window.key}
+              </div>
+            );
+          })}
+        </div>
+        <div className={"h-12 bg-black"}></div>
+      </div>
+    );
 }
