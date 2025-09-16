860ff3fead5a0b482e662343e775b7afe87b5485
adamcanard <azcunard@gmail.com>
2025-09-15 23:04:15 -0600

    style: moved

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -21,19 +21,35 @@ function StartMenu() {
   if (startMenu)
     return (
       <div className={"flex flex-col absolute w-96 h-1/2 bg-blue-500 bottom-8"}>
-        <div
-          className={"grid grid-cols-3 grid-rows-4 h-full w-full bg-blue-200"}
-        >
-          {Object.values(windowRecord).map((window) => {
-            return (
-              <div
-                className={"w-full h-full border-2 border-black"}
-                key={window.window.key}
-              >
-                {window.window.key}
-              </div>
-            );
-          })}
+        <div className={"h-18 bg-black"}></div>
+        <div className={"flex flex-row h-full w-full bg-blue-200 relative"}>
+          <div className={"flex flex-col h-full w-full gap-2 "}>
+            {Object.values(windowRecord).map((window) => {
+              return (
+                <div
+                  className={"w-full h-12 border-2 hover:bg-blue-500"}
+                  key={window.window.key}
+                >
+                  {window.window.key}
+                </div>
+              );
+            })}
+          </div>
+
+          <div className={"h-full w-full"}>
+            <div className={"flex flex-col h-full w-full gap-2 justify-end"}>
+              {Object.values(windowRecord).map((window) => {
+                return (
+                  <div
+                    className={"w-full h-12 border-2 hover:bg-blue-500"}
+                    key={window.window.key}
+                  >
+                    {window.window.key}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
         <div className={"h-12 bg-black"}></div>
       </div>
