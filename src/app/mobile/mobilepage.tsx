7ed43884a390b623a684c8f6f5d070f58b6d78e1
adamcanard <azcunard@gmail.com>
2025-10-26 12:20:40 -0600

    feat: added suspense

--- src/app/mobile/mobilepage.tsx
+++ src/app/mobile/mobilepage.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { Suspense } from "react";
 //import GameContainer from "../testcomps/gamecontainer";
 import Beer from "./beer";
 import Info from "./info";
 import StoreRecipe from "./recipe/storerecipe";
 import { Renderer } from "./renderer/renderer";
+import Loading from "../desktop/sitecomps/loading";
 
 export default function MobilePage() {
   return (
-    <Renderer
-      toRender={{
-        Info: <Info key={"Info"} />,
-        Beer: <Beer key={"Beer"} />,
-        Recipe: <StoreRecipe key={"Recipe"} />,
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <Renderer
+        toRender={{
+          Info: <Info key={"Info"} />,
+          Beer: <Beer key={"Beer"} />,
+          Recipe: <StoreRecipe key={"Recipe"} />,
+        }}
+      />
+    </Suspense>
   );
 }
