e4e132d8c99e4ef5de1a53c9a17745349bde9d0e
adamcanard <azcunard@gmail.com>
2025-11-14 13:48:16 -0700

    test size

--- src/app/mobile/infocomps/beerimage.tsx
+++ src/app/mobile/infocomps/beerimage.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 import Photo from "../../../../public/AdamBeer1.jpg";
 export default function BeerImage() {
   return (
-    <div
-      id="boxshadow"
-      className={"flex flex-col h-full max-h-1/2"}
-      key={"Photo"}
-    >
+    <div id="boxshadow" className={"flex flex-col h-full"} key={"Photo"}>
       <h1 id="title">Adam Cunard Website. Thats Me!</h1>
       <div className={"relative w-full h-full"}>
         <Image src={Photo} alt="Photo of me" fill={true} />
