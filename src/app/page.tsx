a81357e3abc4c9fe8561fedd4dea5b8aac8d6e25
adamcanard <azcunard@gmail.com>
2025-12-01 15:47:29 -0700

    feat: no more error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Loading from "./loading";
 export default function Page() {
   const router = useRouter();
-  const handleRoute = (route: string) => {
-    router.push(route);
-  };
+
   useEffect(() => {
     const mql = window.matchMedia("(max-width: 1300px)");
     if (mql.matches) {
       router.push("/mobile");
+    } else {
+      router.push("/98");
     }
   }, [router]);
-  return (
-    <div className={"w-full h-full  justify-center items-center flex"}>
-      <div id="boxshadow" className={"w-1/2 h-1/2 justify-center items-center"}>
-        <h1 id="title" className={"font-bold"}>
-          {"Welcome to Adam's Portfolio"}
-        </h1>
-        <button id="button" onClick={() => handleRoute("xp")}>
-          Windows XP
-        </button>
-        <button id="button" onClick={() => handleRoute("98")}>
-          Windows 98
-        </button>
-      </div>
-    </div>
-  );
+
+  return <Loading />;
 }
+//<div className={"w-full h-full  justify-center items-center flex"}>
+//      <div id="boxshadow" className={"w-1/2 h-1/2 justify-center items-center"}>
+//        <h1 id="title" className={"font-bold"}>
+//          {"Welcome to Adam's Portfolio"}
+//        </h1>
+//        <button id="button" onClick={() => handleRoute("xp")}>
+//          Windows XP
+//        </button>
+//        <button id="button" onClick={() => handleRoute("98")}>
+//          Windows 98
+//        </button>
+//      </div>
+//    </div>
