8bc4dd2888fbe894e7cd2fce8ab2f144d533bf12
adamcanard <azcunard@gmail.com>
2026-03-01 16:03:24 -0700

    comment

--- src/app/api/expense/route.ts
+++ src/app/api/expense/route.ts
@@ -1,25 +1,25 @@
 //import { IParty } from "@/app/mobile/expensecomps/expense";
 //import { Expense } from "@/app/server/models/expense";
 //import connectMongo from "@/app/server/mongoose";
-//import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 //
-//export async function POST(req: Request) {
-//  const formData = await req.formData();
-//  const party: IParty = {
-//
-//  };
-//
-//  try {
-//    await connectMongo();
-//    const newExpense = new Expense(party);
-//    const returnedExpense = await newExpense.save();
-//    console.log(returnedExpense);
-//    return NextResponse.json(
-//      { returnedExpense, message: "Your product has been created" },
-//      { status: 201 },
-//    );
-//  } catch (error) {
-//    console.log(error);
-//    return NextResponse.json({ message: error }, { status: 400 });
-//  }
-//}
+export async function POST(req: Request) {
+  //  const formData = await req.formData();
+  //  const party: IParty = {
+  //
+  //  };
+  //
+  //  try {
+  //    await connectMongo();
+  //    const newExpense = new Expense(party);
+  //    const returnedExpense = await newExpense.save();
+  //    console.log(returnedExpense);
+  //    return NextResponse.json(
+  //      { returnedExpense, message: "Your product has been created" },
+  //      { status: 201 },
+  //    );
+  //  } catch (error) {
+  console.log(req);
+  return NextResponse.json({ message: "" }, { status: 400 });
+  //  }
+}
